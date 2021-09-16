ac434c5c047e79c610bdd623a3569808dafdf59f
kavish-mobileapp <74291513+kavish-mobileapp@users.noreply.github.com>
2021-09-16 14:22:21 +0530

    Add files via upload

---
+++ C37RV_SpeedRacer_ReferenceCode-main/sketch.js
@@ -0,0 +1,31 @@
+var canvas;
+var backgroundImage, bgImg, car1_img, car2_img, track;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers, car1, car2;
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("assets/background.png");
+  
+
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  
+
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
